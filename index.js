2e9e045b0f2fe85fc48b5dfe306a2a5956b226d6
Issei Horie <issei.horie@is2ei.com>
2022-01-30 21:55:38 +0900

    add .editorconfig

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const requestListener = function (req, res) {
   const { window } = new JSDOM(`...`);
   const DOMPurify = createDOMPurify(window);
 
-  const clean = DOMPurify.sanitize(decodeURI(queryObject['q']));  
+  const clean = DOMPurify.sanitize(decodeURI(queryObject['q']));
 
   const responseBody = `
 <html>
@@ -21,7 +21,7 @@ const requestListener = function (req, res) {
     <h1>DOMPurify version: ${createDOMPurify.version}</h1>
     ${clean != null? clean : ''}
   </body>
-</html>  
+</html>
   `
   res.writeHead(200);
   res.end(responseBody);
