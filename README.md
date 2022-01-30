a7e617b8eca85bb9c70604eea93c83fae4424e9e
Issei Horie <issei.horie@is2ei.com>
2022-01-30 20:34:22 +0900

    add tiny web app

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# DOMPurify-fuzzing-test
+DOMPurify-fuzzing-test
+===
+
+
+## Getting started
+
+#### 1) Install dependencies
+
+```shell
+npm i
+```
+
+#### 2) Run server
+
+```shell
+npm start
+```
+
+#### 3) Access
+
+http://localhost
---
+++ index.js
@@ -0,0 +1,31 @@
+const createDOMPurify = require('dompurify');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const http = require('http');
+const url = require('url');
+
+const requestListener = function (req, res) {
+  const queryObject = url.parse(req.url, true).query;
+
+  const { window } = new JSDOM(`...`);
+  const DOMPurify = createDOMPurify(window);
+
+  const clean = DOMPurify.sanitize(decodeURI(queryObject['q']));  
+
+  const responseBody = `
+<html>
+  <head>
+  </head>
+  <body>
+    <h1>DOMPurify version: ${createDOMPurify.version}</h1>
+    ${clean != null? clean : ''}
+  </body>
+</html>  
+  `
+  res.writeHead(200);
+  res.end(responseBody);
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
